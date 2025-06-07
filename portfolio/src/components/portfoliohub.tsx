173c4b7cbd7b178294574ad228f57f6d916a685d
Tyran Rice Jr <tyranjr@udel.edu>
2025-06-07 14:00:35 -0400

    Added titles to each planet

--- portfolio/src/components/portfoliohub.tsx
+++ portfolio/src/components/portfoliohub.tsx
@@ -22,17 +22,29 @@ export const PortfolioHub = () => {
   return (
   <div className="h-screen w-screen overflow-hidden absolute z-10 text-white animate-fade-in p-8">
       <div className="max-w-1/3 relative top-1/3 left-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-center">
-          <h1 className="text-4xl font-bold underline mb-2">Tyran Rice Jr.</h1>
-          <h2 className="text-xl font bold">Software Engineer & Game Developer</h2>
-          <h2 className="text-xl font bold">Newark, Delaware, United States</h2>
+          <h1 className="text-4xl font-black underline mb-2">Tyran Rice Jr.</h1>
+          <h2 className="text-xl font-bold">Software Engineer & Game Developer</h2>
+          <h2 className="text-xl font-bold">Newark, Delaware, United States</h2>
       </div>
-      <div className="-translate-y-32 z-20">
-        <img className="absolute scale-25 -translate-x-3/8 -translate-y-3/8 animate-spin-slowest" 
-        src="./planets/planet1.png" alt="Planet 1" onClick={() => handleOpen("modal1")}></img>
-        <img className="absolute scale-30 translate-x-1/3 -translate-y-1/4 animate-spin-slower" 
-        src="./planets/planet2.png" alt="Planet 2" onClick={() => handleOpen("modal2")}></img>
-        <img className="absolute scale-25 -translate-x-1/8 -translate-y-1/8 animate-spin-slow" 
-        src="./planets/planet3.png" alt="Planet 3" onClick={() => handleOpen("modal3")}></img>
+      <div className="-translate-y-32 z-20 text-center text-9xl font-black">
+        <div className="absolute scale-25 -translate-x-3/8 -translate-y-3/8" onClick={() => handleOpen("modal1")}>
+          <h1 className="absolute h-1/2 w-full translate-y-3/4 z-20 drop-shadow-[2px_2px_8px_rgba(0,0,0,0.8)]">
+            Projects
+          </h1>
+          <img className="animate-spin-slowest" src="./planets/planet1.png" alt="Planet 1"></img>
+        </div>
+        <div className="absolute scale-30 translate-x-1/3 -translate-y-1/4" onClick={() => handleOpen("modal2")}>
+          <div className="absolute h-1/2 w-full translate-y-3/4 z-20 drop-shadow-[2px_2px_8px_rgba(0,0,0,0.8)]">
+            N/A
+          </div>
+          <img className="animate-spin-slower" src="./planets/planet2.png" alt="Planet 2"></img>
+        </div>
+        <div className="absolute scale-25 -translate-x-1/8 -translate-y-1/8" onClick={() => handleOpen("modal3")}>
+          <div className="absolute h-1/2 w-full translate-y-3/4 z-20 drop-shadow-[2px_2px_8px_rgba(0,0,0,0.8)]">
+            Skills
+          </div>
+          <img className="animate-spin-slow" src="./planets/planet3.png" alt="Planet 3"></img>
+        </div>
       </div>
 
       {openModal === "modal1" && <Planet1Modal onClose={handleClose} />}
