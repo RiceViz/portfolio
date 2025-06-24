b42caae0462503e7b6fb9905f055e3737559d024
Tyran Rice Jr <tyranjr@udel.edu>
2025-06-23 20:26:15 -0400

    Fixed Planets and Title UI

--- portfolio/src/components/portfoliohub.tsx
+++ portfolio/src/components/portfoliohub.tsx
@@ -23,30 +23,30 @@ export const PortfolioHub = () => {
   return (
   <div className="h-screen w-screen overflow-hidden absolute z-10 text-white animate-fade-in p-8 transition duration-150 ease-in-out">
       {/*Title*/}
-      <div className="lg:max-w-1/2 relative lg:top-1/5 lg:left-1/2 z-10 lg:-translate-x-1/2 lg:-translate-y-1/5 text-center">
+      <div className="lg:max-w-2/3 relative lg:top-2/7 lg:left-1/2 z-10 lg:-translate-x-1/2 lg:-translate-y-1/5 text-center">
           {/*<img className="fixed rounded-full border-8 scale-25 -translate-y-5/8" src="./portfoliopicture.jpeg" alt="Picture Of Me"></img>*/}
-          <AuroraText className="text-5xl lg:text-6xl font-black mb-2 drop-shadow-[2px_2px_8px_rgba(255,255,255,0.4)]">Tyran Rice Jr.</AuroraText>
+          <AuroraText className="text-5xl lg:text-7xl font-black mb-2 drop-shadow-[2px_2px_8px_rgba(255,255,255,0.4)]">Tyran Rice Jr.</AuroraText>
           <h2 className="text-lg lg:text-xl font-bold drop-shadow-[2px_2px_8px_rgba(255,255,255,0.4)]">Software Engineer & Game Developer</h2>
           <h2 className="text-lg lg:text-xl font-bold drop-shadow-[2px_2px_8px_rgba(255,255,255,0.4)]">Delaware, United States</h2>
       </div>
 
       {/*Planets*/}
       <div className="grid grid-cols-3 lg:-translate-y-32 lg:z-20 text-center text-5xl lg:text-9xl font-black">
-        <div className="lg:absolute scale-50 lg:scale-25 lg:-translate-x-3/8 lg:-translate-y-5/12 transition delay-100 duration-150 ease-in-out  hover:scale-55 lg:hover:scale-30" 
+        <div className="lg:absolute scale-50 lg:scale-25 lg:-translate-x-2/5 lg:-translate-y-2/5 transition delay-100 duration-150 ease-in-out  hover:scale-55 lg:hover:scale-30" 
         onClick={() => handleOpen("projects")}>
           <h1 className="lg:absolute h-1/2 w-full lg:translate-y-3/4 z-20 drop-shadow-[2px_2px_8px_rgba(0,0,0,0.8)]">
             Projects
           </h1>
           <img className="animate-spin-slowest" src="./planets/planet1.png" alt="Planet 1"></img>
         </div>
-        <div className="lg:absolute scale-50 lg:scale-30 lg:translate-x-1/3 lg:-translate-y-1/3 transition delay-100 duration-150 ease-in-out hover:scale-55 lg:hover:scale-35" 
+        <div className="lg:absolute scale-50 lg:scale-30 lg:translate-x-3/8 lg:-translate-y-1/4 transition delay-100 duration-150 ease-in-out hover:scale-55 lg:hover:scale-35" 
         onClick={() => handleOpen("about me")}>
           <div className="lg:absolute h-1/2 w-full lg:translate-y-3/4 z-20 drop-shadow-[2px_2px_8px_rgba(0,0,0,0.8)]">
             About Me
           </div>
           <img className="animate-spin-slower" src="./planets/planet2.png" alt="Planet 2"></img>
         </div>
-        <div className="lg:absolute scale-50 lg:scale-25 lg:-translate-x-1/8 lg:-translate-y-[225px] transition delay-100 duration-150 ease-in-out hover:scale-55 lg:hover:scale-30" 
+        <div className="lg:absolute scale-50 lg:scale-25 lg:-translate-x-1/12 lg:-translate-y-[150px] transition delay-100 duration-150 ease-in-out hover:scale-55 lg:hover:scale-30" 
         onClick={() => handleOpen("socials")}>
           <div className="lg:absolute h-1/2 w-full lg:translate-y-3/4 z-20 drop-shadow-[2px_2px_8px_rgba(0,0,0,0.8)]">
             Socials
